2e656bdd1355f2b5c2c003914a734c2718a4f9ca
RyanRizzo96 <31866965+RyanRizzo96@users.noreply.github.com>
2017-10-13 18:33:40 +0200

    Testing Dijkstra function

--- DIjkstra Implementation/Dijkstra.cpp
+++ DIjkstra Implementation/Dijkstra.cpp
@@ -9,9 +9,40 @@ ifstream fin("a.in");	//input file
 
 int N;					//number of nodes
 int a, b, w;			//Node a to Node b and weight w
-int am[100][100];	
+int am[100][100];
 int distances[100];		//1-D array distance
 int visited[100];		//Set initally to 0
+
+//dijkstra function. gets current node
+int dij(int x) {
+	visited[x] = 1;		//sets current node as visited, marked as 1. They were all 0 before
+
+	//updates the nodes
+	for (int i = 1; i <= N; i++) {		//for all the nodes
+		//check if they are not visited and they are connected
+		if ((visited[i] == 0) && (am[x][i] != -1)) {		
+			//if distance of x + matrix is more than the distance that i already has or if distance of i is infinity, update
+			if ((distances[x] + am[x][i] < distances[i]) || (distances[i] == -1)) {
+				distances[i] = distances[x] + am[x][i];		//update i
+			}
+		}
+	}
+	//after updating all the nodes, we are going to select a new node
+	int smaller = -1, smaller_weight = -1;
+	//for all the nodes, going to check which will be the next node
+	for (int i = 0; i <= N; i++) {
+		//node should not be visited and the distane should be greater than 0. smaller_weight should be bigger than current distance or esle equal to -1
+		if ((visited[i] == 0) && (distances[i] >= 0) && ((smaller_weight > distances[i]) || (smaller_weight == -1))) {
+			smaller_weight = distances[i];	//
+			smaller = i;
+		}
+	}
+	if (smaller != 1) {
+		dij(smaller);
+		return 0;
+	}
+	//return 0;
+}
 
 int main() {
 	//reading number of nodes
