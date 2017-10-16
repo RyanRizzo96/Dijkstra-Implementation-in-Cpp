cc0fa9ba92f151cbf6e521dfc5ebc53ca86f40c1
RyanRizzo96 <31866965+RyanRizzo96@users.noreply.github.com>
2017-10-16 06:56:21 +0200

    Successfully printing shortest Paths

--- DIjkstra Implementation/Dijkstra.cpp
+++ DIjkstra Implementation/Dijkstra.cpp
@@ -29,15 +29,16 @@ int node_matrix[100][100];	//2D node matrix
 int path_matrix[100][100];	//2D matrix to store paths
 int distances[100];		//1-D array distance
 int visited[100];		//Set initally to 0
+vector<int> path;
 
 //dijkstra function. gets current node
 int dijkstra(int x) {
 	visited[x] = 1;		//sets current node as visited, marked as 1. They were all 0 before
 
-	//updates the nodes
+						//updates the nodes
 	for (int i = 1; i <= N; i++) {		//for all the nodes (1 to 6)
-		//check if they are not visited and they are connected
-		if ((visited[i] == 0) && (node_matrix[x][i] != -1)) {		
+										//check if they are not visited and they are connected
+		if ((visited[i] == 0) && (node_matrix[x][i] != -1)) {
 			//if distancefrom node 1 (0) + distance in node matrix of node + next node is less than the distance in the distances array then this is the new shortest path, update
 			//OR if distance in array distance is -1, update aswell. This will be true for the first round.
 			if ((distances[x] + node_matrix[x][i] < distances[i]) || (distances[i] == -1)) {
@@ -50,8 +51,11 @@ int dijkstra(int x) {
 				//update node with shortest distance
 				path_matrix[x][i] = i;
 			}
+
 		}
 	}
+
+
 	//after updating all the nodes, we are going to select a new node
 	int new_node = -1, smaller_weight = -1;
 	//for all the nodes, going to check which will be the next node
@@ -76,7 +80,7 @@ int main() {
 	//it must be doubled. If the full path is not given, most systems will look in the directory that contains the object program.
 
 	inFile.open("C:\\Users\\ryanr\\Desktop\\MCAST Degree 2\\Engineering Project (2)\\DIjkstra\\DIjkstra Implementation\\dijkstra.txt");
-	
+
 	//Check that the file was opened.For example, the open fails if the file doesn't exist, or if it can't be read 
 	//because another program is writing it.A failure can be detected with code like that below using the !(logical not) operator
 	if (!inFile) {
@@ -106,24 +110,26 @@ int main() {
 	while (!inFile.eof()) {	//reading till end of file 
 		inFile >> a >> b >> w;
 		node_matrix[a][b] = w;		//updating matrix
-		//this line is vital for correct functionality.
-		//make sure to update both paths TO and FROM each node
-		node_matrix[b][a] = w;		
+									//this line is vital for correct functionality.
+									//make sure to update both paths TO and FROM each node
+		node_matrix[b][a] = w;
 	}
 
 	//running dijkstra function
 	dijkstra(1);
-	
+
 	//output
 	for (int i = 1; i <= N; i++) {
-		cout <<"Distance from Node 1 to Node " << i << " is: " << distances[i] << endl;
+		cout << "Distance from Node 1 to Node " << i << " is: " << distances[i] << endl;
 	}
 	cout << endl;
 
+	//the following loop prints out the path_matrix which was used to extract all the shortest paths.
+	//there is enough information in the path matrix to extract the shortest path, however doing so was a problem we encountered.
 	int i = 0;
 	for (int row_node = 1; row_node <= N; row_node++) {
 		for (int col_path = 1; col_path <= N; col_path++) {
-			if (i % 5 == 0) {
+			if (i % 7 == 0) {
 				cout << endl;
 			}
 			i++;
@@ -133,17 +139,39 @@ int main() {
 	cout << endl;
 	cout << endl;
 
-	for (int row_node = 1; row_node <= N; row_node++) {
-		for (int col_path = 1; col_path <= N; col_path++) {
-			if(path_matrix[row_node][col_path] != 0) {
-				if (row_node == 1) {
-					//cout << path_matrix[row_node][col_path] << " <- " << col_path << endl;
-					cout << row_node<< " -> " << path_matrix[row_node][col_path] << endl;
+	/* ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ **/
+	//extracting shortest paths from path_matrix
+	int preservedNodeState = 0;
+	for (int searchNodePath = N; searchNodePath >= 1; searchNodePath--) { //rows
+		for (int col_path = N; col_path >= 1; col_path--) {
+			if (path_matrix[searchNodePath][col_path] != 0) {
+
+				path.push_back(path_matrix[searchNodePath][col_path]);
+				path.push_back(searchNodePath);
+
+				//save the searchNodePath row we are currently on as we will be altering this in the following loop.
+				preservedNodeState = searchNodePath;
+
+				for (int row_previousNode = N; row_previousNode >= 1; row_previousNode--) {
+					for (int col_path2 = N; col_path2 >= 1; col_path2--) {
+						if (path_matrix[row_previousNode][col_path2] == searchNodePath) {
+							path.push_back(row_previousNode);
+							//in order to keep on searching through the path matrix
+							searchNodePath = row_previousNode; //set row_previousNode which is the previous node in the path to row_searchnodePath which we are looking for
+							row_previousNode = N; //reset row_node2 to number of nodes present, to start search from beginning
+							col_path2 = N; //reset col_path2 to number of nodes present, to start search from beginning
+						}
+					}
 				}
-				else {
-					//cout << path_matrix[row_node][col_path] << " <- " << col_path << " <- 1" << endl;
-					cout << "1 -> " << row_node << " -> " << path_matrix[row_node][col_path] << endl;
+				//print out the path vector. path.size() gives current vector size.
+				for (int i = 0; i < path.size(); i++) {
+					cout << path[i] << " ";
 				}
+				cout << endl;
+				//.clear() removes all elements of the current path, since we are now starting a new path
+				path.clear();
+				//the searchNodePath which was altered, now retains its value in the original pathMatrix
+				searchNodePath = preservedNodeState;
 			}
 		}
 	}
