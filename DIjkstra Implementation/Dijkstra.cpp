fd72b8351b4f2ee84b7f5d8b36362c59049819cf
RyanRizzo96 <31866965+RyanRizzo96@users.noreply.github.com>
2017-11-02 17:26:39 +0100

    Working File Path

--- DIjkstra Implementation/Dijkstra.cpp
+++ DIjkstra Implementation/Dijkstra.cpp
@@ -23,20 +23,18 @@ int finish_node;
 int print_node;
 
 vector<int> extracted_path;
-vector<vector<int>> all_paths(7,  vector<int> (10));
+vector<vector<int>> all_paths(7, vector<int>(10));
 
 void writeFile() {
 	int nodes;
 	//int input;
 	int input_count = 0;
 
-	//outFile.open("c:\\Users\\Marka\\Documents\\MCAST Degree 2nd year\\Final Year Project\\Software\\project soft\\Engineering-Project\\dijkstra3.txt");
-
-	inFile.open("C:\\Users\\ryanr\\Desktop\\MCAST Degree 2\\Engineering Project (2)\\DIjkstra\\dijkstra\\dijkstra3.txt");
+	outFile.open("C:\\Users\\ryanr\\Desktop\\dijkstra\\dijkstra3.txt");
 	
 	if (outFile.is_open())
 	{
-		cout << "Enter Number of nodes present. Enter '0' to exit: " << endl;
+		cout << "Enter Number of nodes present. Enter '0' to exit: "  << endl;
 		cin >>  nodes;
 		outFile << nodes << endl;
 
@@ -59,7 +57,10 @@ void writeFile() {
 		}
 		cout << endl;
 	}
-	else cout << "Unable to open file\n" << endl;
+	else {
+		cout << "Unable to open file\n" << endl;
+		cout << errno;
+	}
 	outFile.close();
 }
 
@@ -67,7 +68,9 @@ void readFile() {
 	//Open the file stream. Path names in MS Windows use backslashes (\). Because the backslash is also the string escape character, 
 	//it must be doubled. If the full path is not given, most systems will look in the directory that contains the object program.
 
-	inFile.open("C:\\Users\\ryanr\\Desktop\\MCAST Degree 2\\Engineering Project (2)\\DIjkstra\\dijkstra\\dijkstra3.txt");
+
+	inFile.open("C:\\Users\\ryanr\\Desktop\\dijkstra\\dijkstra3.txt");
+
 
 	//Check that the file was opened.For example, the open fails if the file doesn't exist, or if it can't be read 
 	//because another program is writing it.A failure can be detected with code like that below using the !(logical not) operator
