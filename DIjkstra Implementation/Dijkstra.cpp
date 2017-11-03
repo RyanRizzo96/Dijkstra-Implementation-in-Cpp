ca505b5f854f7936272bb8dd8caaef771dab96ae
RyanRizzo96 <31866965+RyanRizzo96@users.noreply.github.com>
2017-11-03 10:56:23 +0100

    Version 2.02

--- DIjkstra Implementation/Dijkstra.cpp
+++ DIjkstra Implementation/Dijkstra.cpp
@@ -1,5 +1,5 @@
 // Dijkstra Implementation
-// Version 2.01 
+// Version 2.02
  
 //#include "C:/Program Files/VS2012Schemas/header.txt"	//directory
 
@@ -31,7 +31,8 @@ void writeFile() {
 	//int input;
 	int input_count = 0;
 
-	outFile.open("C:\\Users\\Marka\\Desktop\\dijkstra3");
+	//outFile.open("C:\\Users\\Marka\\Desktop\\dijkstra3");
+	outFile.open("C:\\Users\\ryanr\\Desktop\\dijkstra");
 
 	if (outFile.is_open())
 	{
@@ -69,7 +70,8 @@ void readFile() {
 	//Open the file stream. Path names in MS Windows use backslashes (\). Because the backslash is also the string escape character, 
 	//it must be doubled. If the full path is not given, most systems will look in the directory that contains the object program.
 
-	inFile.open("C:\\Users\\Marka\\Desktop\\dijkstra3");
+	//inFile.open("C:\\Users\\Marka\\Desktop\\dijkstra3");
+	outFile.open("C:\\Users\\ryanr\\Desktop\\dijkstra");
 
 	//Check that the file was opened.For example, the open fails if the file doesn't exist, or if it can't be read 
 	//because another program is writing it.A failure can be detected with code like that below using the !(logical not) operator
